63e2f47469a3c52d6033b6dd080760f88def9900
wangtao <henandaxuewangtao@126.com>
2023-09-16 14:11:20 +0800

    headerview complete

---
+++ Example/JYPageController/JYHaveHeaderView1Controller.swift
@@ -0,0 +1,30 @@
+//
+//  JYHaveHeaderView1Controller.swift
+//  JYPageController_Example
+//
+//  Created by wang tao on 2023/9/16.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class JYHaveHeaderView1Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
